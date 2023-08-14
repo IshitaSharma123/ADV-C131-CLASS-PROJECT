7f6cd5bf5c685ebb4191d0b6126d5e9ca419cd76
Ishita Sharma <110284120+IshitaSharma123@users.noreply.github.com>
2023-08-14 20:45:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ function setup() {
 }
 
 function draw() {
-    Image(img, 0, 0, 640, 420);
+    image(img, 0, 0, 640, 420);
     fill("#FF0000");
-    Text("Dog", 45, 75);
+    text("Dog", 45, 75);
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
